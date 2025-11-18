95b1d9a5221055bf12fe427f346d3455d0aa12c6
Petros Belachew <petrosbelachew@gmail.com>
2025-11-18 20:44:38 +0300

    DB: Mongodb connetion issue

--- server.cjs
+++ server.cjs
@@ -1,16 +1,17 @@
+const env = require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const MongodbUrl = require("env");
 const PORT = process.env.port || 3000;
+
+//Connect to MongoDB
+mongoose
+  .connect(process.env.mongourl)
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-//Connect to MongoDB
-// mongoose
-//   .connect(process.env.)
-//   .then(() => console.log("MongoDB connected!"))
-//   .catch((err) => console.log(err));
