4460c8e2add07c65e2073ee38ed520b52cfced3b
Petros Belachew <petrosbelachew@gmail.com>
2025-11-23 23:59:09 +0300

    feat: add populate function to blog queries

--- src/domains/blog/blog.service.js
+++ src/domains/blog/blog.service.js
@@ -1,17 +1,27 @@
 import blogModel from "./blog.model.js";
 
 class BlogService {
+  // Helper function to define the population query once
+  static getPopulateQuery() {
+    return {
+      path: "writer", // The field in the Blog model to populate
+      select: "name email -_id", // Fields to include from the Writer model, excluding the _id from the writer object
+    };
+  }
+
   async createBlog(data) {
     const Blog = new blogModel(data);
     return await Blog.save(data);
   }
 
   async getAllBlogs() {
-    return await blogModel.find();
+    return await blogModel.find().populate(BlogService.getPopulateQuery());
   }
 
   async getBlogById(id) {
-    return await blogModel.findById(id);
+    return await blogModel
+      .findById(id)
+      .populate(BlogService.getPopulateQuery());
   }
 
   async updateBlog(id, data) {
