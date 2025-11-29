10ef2f6be8e5c3f688306b9036b822236bff1615
Petros Belachew <petrosbelachew@gmail.com>
2025-11-29 23:16:00 +0300

    feat: add cors to app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import blogRoutes from "./routes/blog-routes.js";
 import writerRoutes from "./routes/writer-routes.js";
 
@@ -17,6 +18,12 @@ export function appview() {
   app.get("/health", healthCheck);
   //import itemRoutes from "./model/routes/item-routes.js";
 
+  const allowedOrigins = ["http://localhost:5174"]; // Your frontend URL
+  app.use(
+    cors({
+      origin: allowedOrigins,
+    })
+  );
   // import {blogRoutes,writerRoutes} from "./src/routes"
   app.use("/api/blog", blogRoutes); // All routes in itemRoutes will be prefixed with /api/items
   app.use("/api/writer", writerRoutes); // All routes in itemRoutes will be prefixed with /api/items
